9693701b6004da6a44d0f3c5f1a238f793766a94
denisseai <msdenisseanaya@gmail.com>
2020-04-17 21:47:08 -0700

    Added button

--- src/componets/Student.js
+++ src/componets/Student.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Student = (props) => {
+  const [present, setPresent] = useState(false);
+
   return (
     <div className="Student">
       <h3>{props.fullName}</h3>
       <ul>
-        <li>{props.birthdate}</li>
-        <li>{props.email}</li>
+        <li>Class: C13</li>
+        <li>Birthday: {props.birthdate}</li>
+        <li>Email: {props.email}</li>
       </ul>
+      <button>
+        Mark { present ? 'Absent' : 'Present'}
+      </button>
     </div>
   );
 };
